81d6354aa655f2548ce1bdada71cda2f4860e994
Saurav Prateek <srvptk97@gmail.com>
2025-02-08 12:56:27 +0530

    Allow micrograd to use Tanh activation function dynamically

--- micrograd/nn.py
+++ micrograd/nn.py
@@ -12,19 +12,30 @@ class Module:
 
 class Neuron(Module):
 
-    def __init__(self, nin, nonlin=True):
+    def __init__(self, nin, nonlin=True, use_tanh=False):
         self.w = [Value(random.uniform(-1,1)) for _ in range(nin)]
         self.b = Value(0)
         self.nonlin = nonlin
+        self.use_tanh = use_tanh
 
     def __call__(self, x):
         act = sum((wi*xi for wi,xi in zip(self.w, x)), self.b)
+        
+        if self.use_tanh:
+            # If specified to use a Tanh activation function
+            return act.tanh()
+        
+        # By default uses a relu activation function.
+        # Maybe we can extend this to dynamically to pick activation
+        # functions at the caller end.
         return act.relu() if self.nonlin else act
 
     def parameters(self):
         return self.w + [self.b]
 
     def __repr__(self):
+        if self.use_tanh:
+            return f"{'Tanh' if self.nonlin else 'Linear'}Neuron({len(self.w)})"
         return f"{'ReLU' if self.nonlin else 'Linear'}Neuron({len(self.w)})"
 
 class Layer(Module):
@@ -44,9 +55,9 @@ class Layer(Module):
 
 class MLP(Module):
 
-    def __init__(self, nin, nouts):
+    def __init__(self, nin, nouts, use_tanh=False):
         sz = [nin] + nouts
-        self.layers = [Layer(sz[i], sz[i+1], nonlin=i!=len(nouts)-1) for i in range(len(nouts))]
+        self.layers = [Layer(sz[i], sz[i+1], nonlin=i!=len(nouts)-1, use_tanh=use_tanh) for i in range(len(nouts))]
 
     def __call__(self, x):
         for layer in self.layers:
@@ -57,4 +68,4 @@ class MLP(Module):
         return [p for layer in self.layers for p in layer.parameters()]
 
     def __repr__(self):
-        return f"MLP of [{', '.join(str(layer) for layer in self.layers)}]"
+        return f"MLPO of [{', '.join(str(layer) for layer in self.layers)}]"
